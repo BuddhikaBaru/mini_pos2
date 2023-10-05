50433997eacba90d714b68b9af02b9961d9e2005
BuddhikaBaru <buddhika.baru@gmail.com>
2023-10-05 17:12:36 +0530

    project-authentication-UI-started

---
+++ app/src/main/java/com/posen82645project/mini_pos2/Registration.java
@@ -0,0 +1,14 @@
+package com.posen82645project.mini_pos2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
